fa420311861699df2995f97cbc5a71851be2f2ed
Alejandro Fernández Ruiz <alejandrofernandezruiz8@gmail.com>
2018-03-27 17:03:37 +0200

    Pending from the last commit

---
+++ app/src/main/java/com/graphqlexample/githubgraphqlclient/ui/activities/MainActivity.java
@@ -0,0 +1,46 @@
+package com.graphqlexample.githubgraphqlclient.ui.activities;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+import com.graphqlexample.githubgraphqlclient.R;
+import com.graphqlexample.githubgraphqlclient.data.model.GHCategory;
+import com.graphqlexample.githubgraphqlclient.ui.BaseActivity;
+import com.graphqlexample.githubgraphqlclient.ui.fragments.fragments.SearchFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
+
+    List<GHCategory> categoryItemList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+
+
+        SearchFragment searchFragment = SearchFragment.newInstance(this);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+
+        transaction.replace(R.id.fragment_container, searchFragment);
+        transaction.addToBackStack(null);
+
+// Commit the transaction
+        transaction.commit();
+        categoryItemList = new ArrayList<>();
+    }
+
+
+    public void setSearchList(List<GHCategory> categoryList) {
+        this.categoryItemList = categoryList;
+    }
+
+    public List<GHCategory> getSearchItemList() {
+        return this.categoryItemList;
+    }
+
+}
